ef471c14e49717c39434c42c01473ea4e93cbe9e
Ellie <Jlhanson9@gmail.com>
2021-06-08 19:18:52 -0500

    secondCommit

---
+++ 01-java-fundamentals/01-javaFun/FirstJavaPro.java
@@ -0,0 +1,7 @@
+public class FirstJavaPro {
+    public static void main (String[] args){
+        System.out.println("My name is Jenelle Hanson");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is West Hempstead, NY");
+    }
+}
