b2ac89bce437088225069dfc53f09f9fecaf5a67
Ellie <Jlhanson9@gmail.com>
2021-06-13 11:44:36 -0500

    sixth commit

--- 01-java-fundamentals/05-basics/BasicJavaTest.java
+++ 01-java-fundamentals/05-basics/BasicJavaTest.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class BasicJavaTest {
     public static void main(String[] args) {
         BasicJava bJ = new BasicJava();
@@ -53,12 +50,3 @@ public class BasicJavaTest {
         System.out.println(bJ.shiftingArray(arr7));
     }
 }
-/*
-Shifting the Values in the Array
-Given any array x, say [1, 5, 10, 7, -2], write a method that 
-shifts each number by one to the front. For example, when the 
-method is done, an x of [1, 5, 10, 7, -2] should become 
-[5, 10, 7, -2, 0]. Notice that the last number is 0. The 
-method does not need to wrap around the values shifted out 
-of bounds.
-*/
