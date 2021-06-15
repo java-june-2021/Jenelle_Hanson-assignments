7255a5d45848574523cd9654f72f6e62cce9fcbf
Ellie <Jlhanson9@gmail.com>
2021-06-15 17:04:45 -0500

    eleventh commit

--- 02-java-oop/01-project-class/Project.java
+++ 02-java-oop/01-project-class/Project.java
@@ -1,66 +1,84 @@
 public class Project {
-    //Attributes
+    // Attributes
     private String name;
     private String description;
-    
-
-    //constructor
-    public Project(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-    
-    //method overloading
-    public Project(String name){
-        this.name = name;
-        this.description = "Front-end Project";
-    }
+    private double initialCost;
 
     public Project() {
         this.name = "Website Construction";
         this.description = "Full-Stack Project";
+        this.initialCost = 1000.0;
     }
 
-    //Methods
-    //getters and setters
+    // method overloading
+    public Project(String name) {
+        this.name = name;
+        this.description = "Front-end Project";
+    }
 
-    public String getName(){
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // constructor
+    public Project(String name, String description, double initialCost) {
+        this.name = name;
+        this.description = description;
+        this.initialCost = initialCost;
+    }
+
+    public String elevatorPitch(){
+        return this.name + " (" + this.initialCost + "):" + this.description;
+    }
+
+    // getters and setters
+
+    public String getName() {
         return this.name;
     }
-    
-    public String getDescription(){
-        return this.description;
-    }
-    
-    public String setName(){
-        return this.name;
-    }
-    
-    public String setDescription(){
+
+    public String getDescription() {
         return this.description;
     }
 
+    public double getInitialCost() {
+        return this.initialCost;
+    }
 
-    //empty projects to add a name and 
-    //description to later 
-
-    //projects with just a name
-
-    //projects with both
-
-    //All projects have to get and set both 
-    //properties
-
-    //Elevator pitch: return the name and description separated by a colon
-
-    /*     public void setName(){
-        if(name.equals("")){
-            System.out.println("Please input more characters");
+    public void setName(String name) {
+        if (name.length() < 4) {
+            System.out.println("Name must be longer than 4 characters");
             return;
         }
-        if(name.length() < 4){
-            System.out.println("must be more than 4 characters");
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        if (description.length() < 10) {
+            System.out.println("Description must be longer than 10 characters");
             return;
         }
-     */
+        this.description = description;
+    }
+
+    public void setInitialCost(double initialCost) {
+        if (initialCost < 0) {
+            System.out.println("Initial cost cannot be negative");
+            return;
+        }
+        this.initialCost = initialCost;
+    }
+
+    // empty projects to add a name and
+    // description to later
+
+    // projects with just a name
+
+    // projects with both
+
+    // All projects have to get and set both
+    // properties
+
+    // Elevator pitch: return the name and description separated by a colon
 }
--- 02-java-oop/01-project-class/ProjectTest.java
+++ 02-java-oop/01-project-class/ProjectTest.java
@@ -1,28 +1,51 @@
 public class ProjectTest {
     public static void main(String[] args) {
-        // Project projects = new Project();
-        
-        Project java = new Project();
-        Project javaScript = new Project("User Interface");
-        Project python = new Project("Personal", "Projects for Jobs");
-    
+        Project elevatorPitch = new Project();
+        Project elevatorPitch1 = new Project("User Interface");
+        Project elevatorPitch2 = new Project("Personal", "Projects for Jobs");
+        Project elevatorPitch3 = new Project("Personal", "Projects for Jobs", 1000.0);
 
-        System.out.println(java.getName());
-        System.out.println(java.getDescription());
-        System.out.println(python.getName());
-        System.out.println(python.getDescription());
-        System.out.println(javaScript.getName());
-        System.out.println(javaScript.getDescription());
+
+        System.out.println(elevatorPitch.getName());
+        System.out.println(elevatorPitch.getDescription());
+        System.out.println(elevatorPitch.getInitialCost());
+        System.out.println("*****************");
+
+        System.out.println(elevatorPitch1.getName());
+        System.out.println(elevatorPitch1.getDescription());
+        System.out.println(elevatorPitch1.getInitialCost());
+        System.out.println("*****************");
+
+        System.out.println(elevatorPitch2.getName());
+        System.out.println(elevatorPitch2.getDescription());
+        System.out.println(elevatorPitch2.getInitialCost());
+        System.out.println("*****************");
+
+        System.out.println(elevatorPitch3.getName());
+        System.out.println(elevatorPitch3.getDescription());
+        System.out.println(elevatorPitch3.getInitialCost());
+        System.out.println("*****************");
+
+
+        // System.out.println(elevatorPitch.elevatorPitch());
+        System.out.println(elevatorPitch3.elevatorPitch());
+
     }
 }
-/*  
-    projects.name = "Project1";
-    projects.description = "Create my first project";
-    project1.name = ;
-    project2.name = ;
-    project2.description = ;
-    
-    Java
-    Python
-    Javascript
-*/
+
+/*
+ *  Create a Portfolio class that will keep an ArrayList of Project in the
+ * field projects. Use generics to ensure these are Project objects.
+ * 
+ * 
+ *  Add the appropriate getters/setters/constructors for this class to work
+ * 
+ * 
+ *  Add the getPortfolioCost method that calculates and returns the cost to buy
+ * the entire portfolio.
+ * 
+ * 
+ *  Add the showPortfolio method that will print all the project elevator
+ * pitches, followed by the total cost.
+ * 
+ */
