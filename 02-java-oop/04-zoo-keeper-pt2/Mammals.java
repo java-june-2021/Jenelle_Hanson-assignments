64a3162564b067215f2e6bc915aa137207b45a7c
Ellie <Jlhanson9@gmail.com>
2021-06-18 19:28:40 -0500

    fifteenth commit

--- 02-java-oop/04-zoo-keeper-pt2/Mammals.java
+++ 02-java-oop/04-zoo-keeper-pt2/Mammals.java
@@ -1,15 +1,25 @@
 public class Mammals {
+    private String name;
     private int energyLevel;
+    private boolean canFly;
+
+    //constructor
+    public Mammals(String name, int energyLevel, boolean canFly){
+        this.name = name;
+        this.energyLevel = energyLevel;
+        this.canFly = canFly;
+    }
 
     public int displayEnergy() {
         System.out.println(energyLevel);
         return energyLevel;
     }
 
+    //getters and setters
     public int getEnergyLevel() {
         return this.energyLevel;
     }
-
+    
     public void setEnergyLevel(int energyLevel) {
         if (energyLevel < 0) {
             System.out.println("This animal has no energy and fell asleep.");
@@ -17,4 +27,24 @@ public class Mammals {
         }
         this.energyLevel = energyLevel;
     }
+
+    public int getName() {
+        return this.name;
+    }
+    
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    // public int getEnergyLevel() {
+    //     return this.energyLevel;
+    // }
+    
+    // public void setEnergyLevel(int energyLevel) {
+    //     if (energyLevel < 0) {
+    //         System.out.println("This animal has no energy and fell asleep.");
+    //         return;
+    //     }
+    //     this.energyLevel = energyLevel;
+    // }
 }
