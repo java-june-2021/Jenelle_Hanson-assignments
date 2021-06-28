b24403fbeaa7fefb9110afc2f2ec7c9aba67bd1b
Ellie <Jlhanson9@gmail.com>
2021-06-28 16:43:30 -0500

    twenty secondth commit

--- 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/JenelleHanson/hellohuman/controllers/IndexController.java
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/JenelleHanson/hellohuman/controllers/IndexController.java
@@ -1,15 +1,30 @@
 package com.JenelleHanson.hellohuman.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
-	@RequestMapping("/")
-	public String index() {
-		return "index.jsp";
+	@RequestMapping("")
+	public String index(Model model) {
+			String name = "Human";
+			model.addAttribute("name", name);
+		return "index2.jsp";
 	}
-
-
 	
+	@RequestMapping("/{input}")
+	public String index1(@PathVariable("input") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index2.jsp";
+	}
+	
+	@RequestMapping("/{input}/{input1}")
+	public String index2(@PathVariable("input") String name, @PathVariable("input1") String last, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("last", last);
+		return "index2.jsp";
+	}
 }
--- 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/webapp/WEB-INF/index2.jsp
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/webapp/WEB-INF/index2.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
-    
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,7 +8,7 @@
 		<title>Hello Human</title>
 	</head>
 	<body>
-		<h1></h1>
- 		<c:out value="${'Hello Human'}"/>
+		<h1>Hello <c:out value="${name}"/> <c:out value="${last}"/></h1>
+		<p>Welcome to SpringBoot!</p>
 	</body>
 </html>
